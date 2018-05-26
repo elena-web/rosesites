9d5a1d57fa45a3f8f1927f35878d3342e0def4dd
elena-web <elenadurfeeweb@gmail.com>
2018-05-26 17:30:53 -0600

    er

--- verifyLogin.js
+++ verifyLogin.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkIfUser(){
   if (getCookie("username") == ""){
     window.location="login.html";
@@ -34,40 +33,3 @@ function checkCookie() {
         }
     }
 }
-=======
-function checkIfUser(){
-  if (getCookie("username") == ""){
-    window.location="login.html";
-  }
-  if (getCookie("password") == ""){
-    window.location="login.html";
-  }
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user = getCookie("username");
-    var pass = getCookie("password");
-    if (user == "admin" && pass = "pass1234") {
-        window.location="dashboard.html";
-    } else {
-        window.location="login.html";
-        }
-    }
-}
->>>>>>> 35bcfe5dce32fbb0939679ed63cbd4892c515c2d
