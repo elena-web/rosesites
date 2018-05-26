3f1becce386ececf07981cfcd46366553af33d01
elena-web <36285317+elena-web@users.noreply.github.com>
2018-05-26 16:33:39 -0600

    Add files via upload

---
+++ verifyLogin.js
@@ -0,0 +1,25 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user = getCookie("username");
+    if (user = "admin" && password = "pass1234") {
+        window.location.replace("dashboard.html");
+    } else {
+        window.location.replace("login.html");
+        }
+    }
+}
