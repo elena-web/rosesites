77d86c2dc625449c867a089a366064c669f145dd
elena-web <elenadurfeeweb@gmail.com>
2018-05-28 11:50:52 -0600

    e

--- createLoginCookies.js
+++ createLoginCookies.js
@@ -1,2 +1,36 @@
-createCookie(username, user, 1);
-createCookie(password, pass, 1);
+function getValues() {
+    var user = getElementById("user");
+    var pass = getElementById("pass");
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function createLoginCookies(){
+  if (user != "" && pass != "") {
+    setCookie(username, user, 1);
+    setCookie(password, pass, 1);
+  }
+  else {
+    var emptyElem = document.getElementById("emptyBoxAlert");
+    emptyElem.style.opacity = "100";
+  }
+}
+
+function isCorrectLogin(){
+  if (user == "admin" && pass == "pass1234"){
+    window.location.href="dash.html";
+  }
+  else {
+    var loginElem = document.getElementById("incorrectLoginAlert");
+    loginElem.style.opacity = "100";
+  }
+}
+
+getValues();
+createLoginCookies();
+isCorrectLogin();
