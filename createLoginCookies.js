23a102f427328acdf1980a08e7ee658883575dec
elena-web <elenadurfeeweb@gmail.com>
2018-05-29 17:56:52 -0600

    asd

--- createLoginCookies.js
+++ createLoginCookies.js
@@ -16,8 +16,7 @@ function createLoginCookies(){
     setCookie(password, pass, 1);
   }
   else {
-    var emptyElem = document.getElementById("emptyBoxAlert");
-    emptyElem.style.opacity = "100";
+    document.getElementById("emptyBoxAlert").style.visibility= visible;
   }
 }
 
@@ -26,11 +25,16 @@ function isCorrectLogin(){
     window.location="dash.html";
   }
   else {
-    var loginElem = document.getElementById("incorrectLoginAlert");
-    loginElem.style.opacity = "100";
+    document.getElementById("incorrectLoginAlert").style.visibility = visible;
   }
 }
 
+function hideAlerts(){
+  document.getElementById("incorrectLoginAlert").style.vivisibility = hidden;
+  document.getElementById("emptyBoxAlert").style.vivisibility = hidden;
+}
+
+hideAlerts();
 getValues();
 createLoginCookies();
 isCorrectLogin();
