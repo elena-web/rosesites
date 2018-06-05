f2d494c93c893828359489f2ccf1c84d3af65784
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 16:42:39 -0600

    ad

--- frame.php
+++ frame.php
@@ -7,6 +7,15 @@
       <div class="row">
         <div id="sidebar" class="col-md-2 center-flex">
           <div id="sidebar-content" class="rounded">
+            <ul>
+              <label>DISTRIBUTED CONTROL SYSTEMS</label>
+              <a href="dash.php">Control Rods</a>
+              <a href="dash.php">Condensers</a>
+              <a href="dash.php">Turbines</a>
+              <a href="dash.php">Generator</a>
+              <label>IT SYSTEMS</label>
+              <a href="dash.php">Network Traffic</a>
+            </ul>
           </div>
         </div>
         <div id="content-area" class="col-md-10 center-flex">
