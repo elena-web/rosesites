bb7c9bbc27c3279eb1c10606206ab35e0079e213
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 16:34:55 -0600

    sda

--- frame.php
+++ frame.php
@@ -5,11 +5,11 @@
   <body>
     <div id="frame" class="container-fluid">
       <div class="row">
-        <div id="sidebar" class="col-md-4">
+        <div id="sidebar" class="col-md-4 center-flex">
           <div id="sidebar-content" class="rounded">
           </div>
         </div>
-        <div id="content-area" class="col-md-8">
+        <div id="content-area" class="col-md-8 center-flex">
           <div id="content-area-content" class="rounded">
           </div>
         </div>
