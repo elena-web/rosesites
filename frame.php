4a9c3454430b345e3d85ddbf84f5d5a15ff6441b
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 17:40:44 -0600

    asd

--- frame.php
+++ frame.php
@@ -6,8 +6,8 @@
     <div class="container-fluid h-100">
       <div class="row h-100">
           <aside class="col-12 col-md-2 p-0 bg-light rounded">
-              <nav class="navbar navbar-expand navbar-light bg-light flex-md-column flex-row align-items-start py-2">
-                  <div class="collapse navbar-collapse">
+              <nav class="navbar navbar-expand navbar-light bg-light flex-md-column flex-row align-items-start py-2 rounded">
+                  <div class="collapse navbar-collapse rounded">
                       <ul class="flex-md-column flex-row navbar-nav w-100 justify-content-between">
                           <li class="nav-item">
                               <a class="nav-link pl-0 text-nowrap" href="#"><i class="fa fa-bullseye fa-fw"></i> <span class="font-weight-bold">Brand</span></a>
