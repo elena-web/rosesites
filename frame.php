c81ca0c9a2a2fb5db5eb86c15ca26034c78af711
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 16:31:12 -0600

    afs

--- frame.php
+++ frame.php
@@ -6,8 +6,12 @@
     <div id="frame" class="container-fluid">
       <div class="row">
         <div id="sidebar" class="col-md-4">
+          <div id="sidebar-content" class="rounded">
+          </div>
         </div>
         <div id="content-area" class="col-md-8">
+          <div id="content-area-content" class="rounded">
+          </div>
         </div>
       </div>
     </div>
