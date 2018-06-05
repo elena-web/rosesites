4dbe901ea3e2d11f311c9a08f0819d453bed1c8c
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 16:51:35 -0600

    asd

--- frame.php
+++ frame.php
@@ -8,12 +8,12 @@
         <div id="sidebar" class="col-md-2 center-flex">
           <div id="sidebar-content" class="rounded">
             <ul style="list-style-type:none">
-              <li><label>DISTRIBUTED CONTROL SYSTEMS</label></li>
+              <li><label class="menu-label">DISTRIBUTED CONTROL SYSTEMS</label></li>
               <li><a href="dash.php">Control Rods</a></li>
               <li><a href="dash.php">Condensers</a></li>
               <li><a href="dash.php">Turbines</a></li>
               <li><a href="dash.php">Generator</a></li>
-              <li><label><br/>IT SYSTEMS</label></li>
+              <li><label class="menu-label"><br/>IT SYSTEMS</label></li>
               <li><a href="dash.php">Network Traffic</a></li>
             </ul>
           </div>
