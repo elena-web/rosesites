5dae93ce3e912627acbdfed7227a596831ac859c
elena-web <elenadurfeeweb@gmail.com>
2018-06-06 09:56:46 -0600

    adsf

--- dash_default.php
+++ dash_default.php
@@ -1,7 +1,7 @@
 <p>YOU ARE HOME<br/></p>
 <!--<canvas class="my-4 w-100" id="myChart" width="900" height="380" style="border:1px solid #000000"></canvas>-->
 
-<div style="">
+<div class="" style="height:400px; width:400px;">
   <script>
     window.onload = function (){
       var crc = document.getElementById("controlRodsChart");
