4187b38571cc8730a88ed1d38ed6d6574bc5b037
elena-web <elenadurfeeweb@gmail.com>
2018-06-05 22:11:23 -0600

    fdsa

--- dash_default.php
+++ dash_default.php
@@ -1,3 +1,2 @@
 YOU ARE HOME
-
-<!--<canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>-->
+<canvas class="my-4 w-100" id="myChart" width="900" height="380" style="border:1px solid #000000"></canvas>
