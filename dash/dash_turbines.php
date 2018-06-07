5b0c66ff96d3b6ade8a764d686892d930985b81b
elena-web <elenadurfeeweb@gmail.com>
2018-06-07 13:51:31 -0600

    afds

--- dash/dash_turbines.php
+++ dash/dash_turbines.php
@@ -61,6 +61,7 @@
                   yAxes: [{
                       ticks: {
                           beginAtZero:false
+                        }
                   }]
               }
           }
