82b2f06e15ddd26347b4fe8ff99f816efefbf615
elena-web <elenadurfeeweb@gmail.com>
2018-06-07 12:49:43 -0600

    adsf

--- frame.php
+++ frame.php
@@ -5,20 +5,20 @@
             <div id="sidebar-content" class="rounded">
               <div id="sidebar-content-content">
                 <ul style="list-style-type:none" class="justify-content-left">
-                  <li><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_default.php'">Dashboard Home</button></li>
+                  <li><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_default.php'">Dashboard Home</button></li>
                   <li><label class="menu-label">INDUSTRIAL CONTROL SYS</label></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_fuelRods.php'">Uranium Fuel Rods</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_controlRods.php'">Control Rods</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_temperature.php'">Water Temperature</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_pressurizer.php'">Pressurizer</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_condensers.php'">Condensers</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_turbines.php'">Turbines</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_generator.php'">Generator</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_pumpGenerator.php'">Pump - Generator</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_pumpReactorCooling.php'">Pump - Reactor Cooling</button></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_pressureGaugeReactor.php'">Pressure Gauge - Reactor</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_fuelRods.php'">Uranium Fuel Rods</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_controlRods.php'">Control Rods</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_temperature.php'">Water Temperature</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_pressure.php'">Pressurizer</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_condensers.php'">Condensers</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_turbines.php'">Turbines</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_generator.php'">Generator</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_pumpGenerator.php'">Pump - Generator</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_pumpReactorCooling.php'">Pump - Reactor Cooling</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_pressureGaugeReactor.php'">Pressure Gauge - Reactor</button></li>
                   <li><label class="menu-label"><br/>IT SYSTEMS</label></li>
-                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = 'dash_networkTraffic.php'">Network Traffic</button></li>
+                  <li class="hover-list-item"><button class="btn btn-link" onclick="document.getElementById('mainContent').src = '/dash/dash_networkTraffic.php'">Network Traffic</button></li>
                 </ul>
               </div>
             </div>
@@ -26,7 +26,7 @@
         </div>
         <div id="content-area" class="col center-flex">
           <div id="content-area-content" class="rounded">
-            <iframe id="mainContent" width="100%" height="100%" src="dash_default.php" frameBorder="0">
+            <iframe id="mainContent" width="100%" height="100%" src="/dash/dash_default.php" frameBorder="0">
           </div>
         </div>
       </div>
