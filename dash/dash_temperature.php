97c8ce5ffde34e8b3c403062d71e19b3495b084f
elena-web <elenadurfeeweb@gmail.com>
2018-06-07 13:13:41 -0600

    asdf

--- dash/dash_temperature.php
+++ dash/dash_temperature.php
@@ -62,7 +62,7 @@
               scales: {
                   yAxes: [{
                       ticks: {
-                          beginAtZero:true
+                          beginAtZero:false
                       }
                   }]
               }
